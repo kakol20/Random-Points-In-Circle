1b36ae95365e5fbb139f7b51b33455bad2daad66
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-07-14 19:24:16 +0100

    Add input

--- src/managers/domManager.js
+++ src/managers/domManager.js
@@ -1,20 +1,42 @@
 const DOMManager = (function () {
-	let restartButton = 0;
+	let maxCountText = 0;
+	let maxPointsText = 0;
 
 	return {
+		restartButton: 0,
+		maxCountInput: 0,
+		maxPointsInput: 0,
+
 		preload() {
-			restartButton = createButton('Restart');
-			restartButton.mousePressed(() => {
+			this.restartButton = createButton('Restart');
+			this.restartButton.mousePressed(() => {
 				ProcessManager.changeState('init');
 			});
+
+			maxCountText = createSpan('Points/Frame');
+			this.maxCountInput = createInput(500, 'number');
+			this.maxCountInput.size(75, this.maxCountInput.height);
+
+			maxPointsText = createSpan('Max Points');
+			this.maxPointsInput = createInput(30000, 'number');
+			this.maxPointsInput.size(75, this.maxPointsInput.height);
 		},
 
 		setup() {
-			if (windowWidth > width) {
-				restartButton.position(width + 5, 5);
-			} else {
-				restartButton.position(5, height + 5);
-			}
+			const xPos = windowWidth > width ? width + 5 : 5;
+			let yPos = windowWidth > width ? 5 : height + 5;
+
+			this.restartButton.position(xPos, yPos);
+			yPos += this.restartButton.height + 10;
+
+			maxCountText.position(xPos, yPos + 5);
+			this.maxCountInput.position(xPos + maxCountText.width + 20, yPos);
+			yPos += Math.max(this.maxCountInput.height, maxCountText.height) + 10;
+
+			maxPointsText.position(xPos, yPos + 5);
+			this.maxPointsInput.position(xPos + maxPointsText.width + 20, yPos);
+			yPos += Math.max(maxPointsText.height, this.maxPointsInput.height) + 15;
+			console.log(this.maxPointsInput.height);
 		}
 	}
 })()
--- src/timing.js
+++ src/timing.js
@@ -1,14 +1,20 @@
 const Timing = (function () {
 	let startTime = 0;
+	let count = 0;
 	return {
 		maxTime: 0,
+		maxCount: 0,
 		start() {
 			startTime = new Date();
+			count = 0;
+		},
+		iterateCount() {
+			count++;
 		},
 		checkTime() {
 			const elapseTime = (new Date()) - startTime;
 			// if (elapseTime >= maxTime) break;
-			return elapseTime >= this.maxTime;
+			return (elapseTime >= this.maxTime) || (count >= this.maxCount);
 		}
 	}
 })();
